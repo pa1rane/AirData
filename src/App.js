f370bdcc1ef48415b4f2a0653d2687e6782adfa2
Pavan Rane <ranepavanrp@gmail.com>
2023-06-18 15:31:34 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import HashLoader from "react-spinners/HashLoader";
 
 const App = () => {
 
 const [usersList, setUsersList] = useState([]);
-const [isButtonClicked, setIsButtonClicked] = useState(false)
+const [isButtonClicked, setIsButtonClicked] = useState(false);
+const [loading, setLoading] = useState(false);
  
 useEffect(() => {
    isButtonClicked && 
    setTimeout(() => {
    fetchData();
-   }, 2000);
+   }, 3000);
  }, [isButtonClicked]);
 
 const fetchData = async () => {
@@ -26,12 +28,23 @@ const fetchData = async () => {
 };
 const handleFetchData = () => {
     setIsButtonClicked(true);
+    setLoading(true)
+    setTimeout(() =>{
+     setLoading(false)
+    }, 3000)
 };
 console.log(usersList)
   return (
     <>
      <Navbar onFetchData={handleFetchData}/>
      {isButtonClicked && <Users usersList={usersList}/>}
+     <HashLoader
+        color="#36d7b7"
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </>
   )
 }
