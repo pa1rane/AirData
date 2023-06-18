ee2ca5b1d97ba2a777c9fcb52f3f948c8e35268b
Pavan Rane <ranepavanrp@gmail.com>
2023-06-19 00:03:25 +0530

    Formatted the code

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import styles from './Users.module.css';
+import React from "react";
+import styles from "./Users.module.css";
 
-const Users = ({usersList}) => {
+const Users = ({ usersList }) => {
   return (
     <>
       <div className={styles.list}>
         {usersList.map((user) => {
-           return <div className={styles.user} key={user.id}>
-            <img className ={styles.avatar} src={user.avatar} alt={user.first_name + " " + user.last_name} />
-            <p className={styles.name}>{user.first_name + " " + user.last_name}</p>
-            <p className={styles.email}>{user.email}</p>
-           </div>
+          return (
+            <div className={styles.user} key={user.id}>
+              <img
+                className={styles.avatar}
+                src={user.avatar}
+                alt={user.first_name + " " + user.last_name}
+              />
+              <p className={styles.name}>
+                {user.first_name + " " + user.last_name}
+              </p>
+              <p className={styles.email}>{user.email}</p>
+            </div>
+          );
         })}
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
